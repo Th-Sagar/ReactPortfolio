dccd6a839deb25e745a57d01dd3b35f560196bfa
Sagar <86335892+Th-Sagar@users.noreply.github.com>
2023-09-26 21:23:27 +0545

    feat:resume

--- src/components/resume/Resume.jsx
+++ src/components/resume/Resume.jsx
@@ -1,51 +1,97 @@
+import { useState } from "react";
 import Title from "../layouts/Title";
-import ResumeCard from "./ResumeCard";
+import Achievements from "./Achievements";
+import Education from "./Education";
+import Experience from "./Experience";
+import Skills from "./Skills";
 
 const Resume = () => {
+
+  const [educationData, seteducationData] = useState(true)
+  const [skillData, setskillData] = useState(false);
+  const [experienceData, setexperienceData] = useState(false);
+  const [achievementsData, setachievementsData] = useState(false);
+
+
   return (
     <section id="resume" className="w-full py-20 border-b-[1px] border-b-black">
       {" "}
       <div className="flex justify-center items-center text-center">
         <Title
-          title="6+ MONTHS OF EXPERIENCE IN WEB DEVELOPMENT"
+          title="3+ YEARS OF EXPERIENCE IN WEB DEVELOPMENT"
           des="My Resume"
         />
       </div>
       <div>
         <ul className="w-full grid grid-cols-4">
-          <li className="resumeLi">Education</li>
-          <li className="resumeLi">Professional Skills</li>
+          <li
+            onClick={() =>
+              seteducationData(true) &
+              setskillData(false) &
+              setexperienceData(false) &
+              setachievementsData(false)
+            }
+            className={`${
+              educationData
+                ? " border-designColor rounded-lg"
+                : " border-transparent"
+            } resumeLi`}
+          >
+            Education
+          </li>
+          <li
+            onClick={() =>
+              seteducationData(false) &
+              setskillData(true) &
+              setexperienceData(false) &
+              setachievementsData(false)
+            }
+            className={`${
+              skillData
+                ? " border-designColor rounded-lg"
+                : " border-transparent"
+            } resumeLi`}
+          >
+            Professional Skills
+          </li>
 
-          <li className="resumeLi">Experience</li>
-
-          <li className="resumeLi"> Achievements</li>
+          <li
+            onClick={() =>
+              seteducationData(false) &
+              setskillData(false) &
+              setexperienceData(true) &
+              setachievementsData(false)
+            }
+            className={`${
+              experienceData
+                ? " border-designColor rounded-lg"
+                : " border-transparent"
+            } resumeLi`}
+          >
+            Experience
+          </li>
+          <li
+            onClick={() =>
+              seteducationData(false) &
+              setskillData(false) &
+              setexperienceData(false) &
+              setachievementsData(true)
+            }
+            className={`${
+             achievementsData
+                ? " border-designColor rounded-lg"
+                : " border-transparent"
+            } resumeLi`}
+          >
+            {" "}
+            Achievements
+          </li>
         </ul>
       </div>
-      <div className="py-12 font-titleFont">
-        <p className="text-sm text-designColor tracking-[4px] "> 1998 - 2010</p>
-
-        <h2 className="text-4xl font-bold">Education Quality</h2>
-      </div>
-      <div className="mt-14 w-1/2 h-[1000px] border-l-[6px] border-l-black border-opacity-30 flex flex-col gap-10" >
-        <ResumeCard
-          title="Bachelor (BSc. CSIT)"
-          subTitle="Trinity International College (2020-2025)"
-          result="3.45/4.00"
-          des="A bachelor's degree or baccalaureate is an undergraduate academic degree awarded by colleges and universities upon completion of a course of study lasting three to seven years. In some institutions and educational systems, some bachelor's degrees can only be taken as graduate or postgraduate degrees after a first degree has been completed."
-        />
-        <ResumeCard
-          title="+2 Computer Science "
-          subTitle="Oxford College Of Engineering & Management (2017 - 2019) "
-          result="3.85/4.00"
-          des=" Higher education is tertiary education leading to award of an academic degree. Higher education, also called post-secondary education, third-level or tertiary education, is an optional final stage of formal learning that occurs after completion of secondary education."
-        />
-        <ResumeCard
-          title="SEE "
-          subTitle="Marigold English Boarding School (2012-2017) "
-          result="3.95/4.00"
-          des="Secondary education or post-primary education covers two phases on the International Standard Classification of Education scale."
-        />
-      </div>
+      {educationData && <Education />}
+      {skillData && <Skills />}
+      {experienceData && <Experience />}
+      {achievementsData && <Achievements />}
     </section>
   );
 };
---
+++ src/components/resume/Skills.jsx
@@ -0,0 +1,70 @@
+import ResumeCard from "./ResumeCard";
+import { motion } from "framer-motion";
+
+
+const Skills = () => {
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1, transition: { duration: 0.5 } }}
+      className="w-full flex gap-20"
+    >
+      <div>
+        <div className="py-12 font-titleFont">
+          <p className="text-sm text-designColor tracking-[4px] "> Features</p>
+
+          <h2 className="text-4xl font-bold">Design Skill</h2>
+        </div>
+        <div className="mt-14 w-full h-[1000px] border-l-[6px] border-l-black border-opacity-30 flex flex-col gap-10">
+          <ResumeCard
+            title="Bachelor (BSc. CSIT)"
+            subTitle="Trinity International College (2020-2025)"
+            result="Processing"
+            des="A bachelor's degree or baccalaureate is an undergraduate academic degree awarded by colleges and universities upon completion of a course of study lasting three to seven years. In some institutions and educational systems, some bachelor's degrees can only be taken as graduate or postgraduate degrees after a first degree has been completed."
+          />
+          <ResumeCard
+            title="+2 Computer Science "
+            subTitle="Oxford College Of Engineering & Management (2017 - 2019) "
+            result="3.85/4.00"
+            des=" Higher education is tertiary education leading to award of an academic degree. Higher education, also called post-secondary education, third-level or tertiary education, is an optional final stage of formal learning that occurs after completion of secondary education."
+          />
+          <ResumeCard
+            title="SEE "
+            subTitle="Marigold English Boarding School (2012-2017) "
+            result="3.95/4.00"
+            des="Secondary education or post-primary education covers two phases on the International Standard Classification of Education scale."
+          />
+        </div>
+      </div>
+      <div>
+        <div className="py-12 font-titleFont">
+          <p className="text-sm text-designColor tracking-[4px] "> Features</p>
+
+          <h2 className="text-4xl font-bold">Development Skill</h2>
+        </div>
+        <div className="mt-14 w-full h-[1000px] border-l-[6px] border-l-black border-opacity-30 flex flex-col gap-10">
+          <ResumeCard
+            title="Sr. software Engineer"
+            subTitle="Google Out Tech (2022-Present)"
+            result="USA"
+            des="Google's hiring process is an important part of our culture. Googlers care deeply about their teams and the people who make them up. We also care about the candidates who interview with us. We want to make sure that you have a positive experience at Google, where we believe in respectful interviews, and where you can expect to hear from us in a timely manner.."
+          />
+          <ResumeCard
+            title="Web Developer & Trainer"
+            subTitle="Apple Developer Team - (2021-2022) "
+            result="Netherlands"
+            des=" A popular destination with a growing number of highly qualified homegrown graduates, it's true that securing a job in the Netherlands can be a challenge. However, with persistence and patience, it's possible to find a job within three to six months of arriving in the country."
+          />
+          <ResumeCard
+            title="Front-end Developer "
+            subTitle="Microsoft (2019-2021) "
+            result="Australia"
+            des="Microsoft Corporation is an American multinational technology company with headquarters in Redmond, Washington. It develops, manufactures, licenses, supports, and sells computer software, consumer electronics, personal computers, and related services."
+          />
+        </div>
+      </div>
+    </motion.div>
+  );
+};
+
+export default Skills;
