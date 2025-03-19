d16f375fba66e085799ee3c7c58091362b059519
Th-Sagar <sagarthapa98122@gmail.com>
2025-03-19 21:28:44 +0545

    feat:

--- src/components/resume/Education.jsx
+++ src/components/resume/Education.jsx
@@ -18,7 +18,7 @@ const Education = () => {
             2003 - 2025
           </p>
 
-          <h2 className="text-3xl md:text-4xl font-bold">Education Quality</h2>
+          <h2 className="text-3xl md:text-4xl font-bold">Education</h2>
         </div>
         <div className=" mt-6 lgl:mt-14 w-full h-[1000px] border-l-[6px] border-l-black border-opacity-30 flex flex-col gap-10">
           <ResumeCard
